499adacdee4dd89b8a710855fe83d6219a882cfa
Weslley Alves de Oliveira <wesshead@gmail.com>
2019-06-10 10:09:27 -0300

    Creates the app herdar using material-ui components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="x-app">
+      <AppBar position="static" className="x-header">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="h1"
+            className="x-header--title"
+            noWrap
+          >
+            Schedule your massege
+          </Typography>
+          <IconButton
+            aria-label="Account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+            edge="end"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
