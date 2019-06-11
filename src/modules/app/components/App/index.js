9370f8d93867a1f0e8c5432ef3ea379c56cd4833
Weslley Alves de Oliveira <wesshead@gmail.com>
2019-06-11 11:58:42 -0300

    Creates the app module with the app component

---
+++ src/modules/app/components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Header } from '..'
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="x-app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/app/components/Header/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const Header = () => (
+  <AppBar position="static" className="x-header">
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="Menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography
+        variant="h6"
+        component="h1"
+        className="x-header--title"
+        noWrap
+      >
+        Schedule your massege
+      </Typography>
+      <IconButton
+        aria-label="Account of current user"
+        aria-controls="primary-search-account-menu"
+        aria-haspopup="true"
+        color="inherit"
+        edge="end"
+      >
+        <AccountCircle />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
+)
+
+export default Header
